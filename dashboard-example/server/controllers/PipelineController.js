47e326ebc4b64c7dcf8fb1a11989b7f83fe3b03e
Guillem Blanché <guillem.blanche@invoicesharing.com>
2017-10-26 17:52:33 +0200

    Initial implementation with option to serve remote webs

--- dashboard-example/server/controllers/PipelineController.js
+++ dashboard-example/server/controllers/PipelineController.js
@@ -5,7 +5,7 @@ import Promise from 'bluebird';
 export default class PipelineController {
 
   handle() {
-    return axios.get('https://jenkins.acn-integration.invoicesharing.com/blue/organizations/jenkins/GUI%2FCI-Multibranch/activity').then((res) => {
+    return axios.get('https://jenkins.url/blue/organizations/jenkins/GUI%2FCI-Multibranch/activity').then((res) => {
       return nunjucks.renderString((response))
     });
   }
